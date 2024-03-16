5b7f591966a4adca157539346331e3d8d68e182f
TheLedgerOfJoudi <hadi.mohammad.saleh1@gmail.com>
2024-03-16 16:40:49 +0300

    add mainnet metadata

--- node/all.js
+++ node/all.js
@@ -14,6 +14,14 @@ const secretjs = new SecretNetworkClient({
     walletAddress: wallet.address,
 });
 
+// uncomment for mainnet
+// const secretjs = new SecretNetworkClient({
+//     chainId: "secret-4",
+//     url: "https://lcd.mainnet.secretsaturn.net",
+//     wallet: wallet,
+//     walletAddress: wallet.address,
+// });
+
 
 let codeId, contractCodeHash, contractAddress
 
@@ -72,70 +80,70 @@ let instantiate_contract = async () => {
 
 await instantiate_contract();
 
-let add_epoch = async () => {
-    const owner = "0x244897572368eadf65bfbc5aec98d8e5443a9072"
+// let add_epoch = async () => {
+//     const owner = "0x244897572368eadf65bfbc5aec98d8e5443a9072"
 
-    let tx1 = await secretjs.tx.compute.executeContract(
-        {
-            sender: wallet.address,
-            contract_address: contractAddress,
-            msg: {
-                add_epoch: {
-                    witness: [{ address: owner, host: "" }],
-                    minimum_witness: "1",
-                }
-            },
-            code_hash: contractCodeHash,
-        },
-        { gasLimit: 100_000 }
-    );
-    // console.log(tx1)
-};
+//     let tx1 = await secretjs.tx.compute.executeContract(
+//         {
+//             sender: wallet.address,
+//             contract_address: contractAddress,
+//             msg: {
+//                 add_epoch: {
+//                     witness: [{ address: owner, host: "" }],
+//                     minimum_witness: "1",
+//                 }
+//             },
+//             code_hash: contractCodeHash,
+//         },
+//         { gasLimit: 100_000 }
+//     );
+//     // console.log(tx1)
+// };
 
-await add_epoch();
+// await add_epoch();
 
-let verify_proof = async () => {
-    const owner = "0xe4c20c9f558160ec08106de300326f7e9c73fb7f"
+// let verify_proof = async () => {
+//     const owner = "0xe4c20c9f558160ec08106de300326f7e9c73fb7f"
 
-    const claimInfo = {
-        "provider": "http",
-        "parameters": "{\"body\":\"\",\"geoLocation\":\"in\",\"method\":\"GET\",\"responseMatches\":[{\"type\":\"contains\",\"value\":\"_steamid\\\">Steam ID: 76561199632643233</div>\"}],\"responseRedactions\":[{\"jsonPath\":\"\",\"regex\":\"_steamid\\\">Steam ID: (.*)</div>\",\"xPath\":\"id(\\\"responsive_page_template_content\\\")/div[@class=\\\"page_header_ctn\\\"]/div[@class=\\\"page_content\\\"]/div[@class=\\\"youraccount_steamid\\\"]\"}],\"url\":\"https://store.steampowered.com/account/\"}",
-        "context": "{\"contextAddress\":\"user's address\",\"contextMessage\":\"for acmecorp.com on 1st january\"}",
-    }
+//     const claimInfo = {
+//         "provider": "http",
+//         "parameters": "{\"body\":\"\",\"geoLocation\":\"in\",\"method\":\"GET\",\"responseMatches\":[{\"type\":\"contains\",\"value\":\"_steamid\\\">Steam ID: 76561199632643233</div>\"}],\"responseRedactions\":[{\"jsonPath\":\"\",\"regex\":\"_steamid\\\">Steam ID: (.*)</div>\",\"xPath\":\"id(\\\"responsive_page_template_content\\\")/div[@class=\\\"page_header_ctn\\\"]/div[@class=\\\"page_content\\\"]/div[@class=\\\"youraccount_steamid\\\"]\"}],\"url\":\"https://store.steampowered.com/account/\"}",
+//         "context": "{\"contextAddress\":\"user's address\",\"contextMessage\":\"for acmecorp.com on 1st january\"}",
+//     }
 
-    const identifier = "0x531322a6c34e5a71296a5ee07af13f0c27b5b1e50616f816374aff6064daaf55"
+//     const identifier = "0x531322a6c34e5a71296a5ee07af13f0c27b5b1e50616f816374aff6064daaf55"
 
-    const signedClaim = {
-        "claim": {
-            "identifier": identifier,
-            "owner": owner,
-            "epoch": 1,
-            "timestampS": 1710157447
-        },
-        "signatures": ["0x52e2a591f51351c1883559f8b6c6264b9cb5984d0b7ccc805078571242166b357994460a1bf8f9903c4130f67d358d7d6e9a52df9a38c51db6a10574b946884c1b"],
-    }
+//     const signedClaim = {
+//         "claim": {
+//             "identifier": identifier,
+//             "owner": owner,
+//             "epoch": 1,
+//             "timestampS": 1710157447
+//         },
+//         "signatures": ["0x52e2a591f51351c1883559f8b6c6264b9cb5984d0b7ccc805078571242166b357994460a1bf8f9903c4130f67d358d7d6e9a52df9a38c51db6a10574b946884c1b"],
+//     }
 
 
-    const proof = {
-        claimInfo: claimInfo,
-        signedClaim: signedClaim
-    }
+//     const proof = {
+//         claimInfo: claimInfo,
+//         signedClaim: signedClaim
+//     }
 
-    let tx = await secretjs.tx.compute.executeContract(
-        {
-            sender: wallet.address,
-            contract_address: contractAddress,
-            msg: {
-                verify_proof: {
-                    proof: proof
-                }
-            },
-            code_hash: contractCodeHash,
-        },
-        { gasLimit: 100_000 }
-    );
+//     let tx = await secretjs.tx.compute.executeContract(
+//         {
+//             sender: wallet.address,
+//             contract_address: contractAddress,
+//             msg: {
+//                 verify_proof: {
+//                     proof: proof
+//                 }
+//             },
+//             code_hash: contractCodeHash,
+//         },
+//         { gasLimit: 100_000 }
+//     );
 
-    console.log(tx)
-};
+//     console.log(tx)
+// };
 
-await verify_proof();
+// await verify_proof();
